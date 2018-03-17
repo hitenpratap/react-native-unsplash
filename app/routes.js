68fdb3de6b38ba6f2d42a1614c7ade01b38431d2
Hiten Pratap Singh <hitenpratap99@gmail.com>
2018-03-17 10:14:07 +0530

    Working on authentication

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Router, Stack, Scene} from 'react-native-router-flux';
+import {Router, Stack, Scene, Actions} from 'react-native-router-flux';
 
 import Login from './screens/Login';
 import Signup from './screens/Signup';
@@ -7,14 +7,20 @@ import ErrorPage from './screens/ErrorPage';
 import Home from './screens/Home';
 
 export default class Routes extends Component<{}>{
+    constructor(props){
+        super(props);
+        this.checkAuth = this.checkAuth.bind(this);
+        this.goToLogin = this.goToLogin.bind(this);
+    }
+
     render(){
         return(
             <Router>
                 <Stack key="root" hideNavBar={true}>
-                    <Scene key="login" component={Login} initial={true} />
+                    <Scene key="login" component={Login}/>
                     <Scene key="signup" component={Signup} />
                     <Scene key="errorPage" component={ErrorPage} />
-                    <Scene key="home" component={Home} />
+                    <Scene key="home" component={Home} initial={true} />
                 </Stack>
             </Router>
         )
